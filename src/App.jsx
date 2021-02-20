d223bcdfb12adc74191ef22815b1435ef709d890
A_ito <BoxPistols@users.noreply.github.com>
2021-02-20 22:50:50 +0900

    base Store

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function App() {
+  // render() {
+  return <p>App</p>;
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,44 @@
 // index.js
 import React from "react";
 import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import {} from "./styles.css";
+import App from "./App";
 
-const el = <h2>こんにちは世界</h2>;
-console.log(el);
-
-const title = "こんにちは世界タイトル";
-
-const returnStrings = () => {
-  return "文字列を返す";
+// State
+let state_value = {
+  counter: 0,
+  message: "Counter"
 };
 
-const setComment = () => {
-  return "コメント";
-};
+// Reducer
+function counter(state = state_value, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1,
+        message: "INCREMENT"
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1,
+        message: "DECREMENT"
+      };
+    default:
+      return state;
+  }
+}
+
+// Store作成
+let store = createStore(counter);
 
 const reactElement = (
   <div className="el">
-    <p>{returnStrings()}</p>
-    <p>{setComment()}</p>
-    <p>{Math.random()}</p>
-    <h2>{title}</h2>
+    <h1>React Redux</h1>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </div>
 );
 
